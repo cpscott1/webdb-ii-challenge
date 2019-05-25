14a10bf60e461d4cff12a7ba912fba92d8759353
cpscott1 <thacam18@gmail.com>
2019-05-25 12:43:13 -0500

    added knex and some endpoints

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true,
+}
+
+const db = knex(knexConfig);
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
+
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+  .insert(req.body, 'id')
+  .then(results =. {
+    res.status(200).json(results);
+  })
+  .catch(err => {
+    res.sstatus(500).json(err);
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
